24a98132e588e8935a9a0e6edc26c792327ddf08
dani <dani.zaghian@gmail.com>
2019-08-29 11:19:46 -0700

    adds url segment param to do guess game

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get "/guess_query_url" => "params_examples#guess_query_action"
 
     get "/url_segment/:wildcard" => "params_examples#url_segment_action"
+    get "/guess_segment_url/:user_guess" => "params_examples#guess_query_action"
   end
 end
 
